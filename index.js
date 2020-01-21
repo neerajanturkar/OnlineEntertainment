261de323c33bc8fa00ec0c6b29e271c0f27718cd
Neeraj Anturkar <neeraj@kisan.net>
2020-01-21 16:57:11 +0100

    Setting up Databases

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const redis = require("redis")
+const neo4j = require('neo4j-driver');
+
+const app = express();
+const PORT = 5000;
+const redisClient = redis.createClient();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+
+const driver = neo4j.driver("bolt://localhost:7687", neo4j.auth.basic("neo4j", "Anturkar@05"));
+const session = driver.session();
+mongoose.connect('mongodb://localhost:27017/reale');
+mongoose.connection.on('connected', () => {
+    console.log("Database connected");
+});
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost"); 
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+redisClient.on('connect', function() {
+    console.log('Redis connected');
+});
+// mongoose.connection.console.error('error',(err) => {
+//     if(err){
+//         console.log("Error : " + err);
+//     }
+// });
+
+  
+app.listen(PORT, () => console.log(`Server started on ${PORT}`));
