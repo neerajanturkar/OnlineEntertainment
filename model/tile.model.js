88f479f237cd3263a59ee3572c3003a5bf144b1e
PriyaWorks <priyakumari.de@gmail.com>
2020-01-24 18:55:54 +0100

    Tile model

--- model/tile.model.js
+++ model/tile.model.js
@@ -1,2 +1,14 @@
 const mongoose = require('mongoose');
 
+const tileSchema = mongoose.Schema({
+    tile:	{ type: String, require: true, unique: true },
+    discription: { type : String},
+    type: { type: String },
+    publishedOn: { type: Date},
+    duration: {type: String},
+    genere:	{type: [String]},
+    prevTile: {type: UUID}
+});
+
+const Tile = module.exports = mongoose.model('tile', tileSchema);
+
